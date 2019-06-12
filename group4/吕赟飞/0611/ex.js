80b89bb71f85ad07786c9904d9f2ad4615d4274d
吕赟飞 <lvyunf3@users.noreply.github.com>
2019-06-12 08:53:39 +0800

    吕赟飞0612笔记提交

---
+++ group4/吕赟飞/0611/ex.js
@@ -0,0 +1,16 @@
+
+// for(var i=0;i++ < 100;){
+//     console.log(i);
+// }  //1-100
+
+for(var i=0;;){
+    i++;
+    console.log(i);
+    if(i===100) break;
+}
+
+// var i = 0;
+// while (i++ < 100) {
+//     console.log(i);
+// } //1-100
+
