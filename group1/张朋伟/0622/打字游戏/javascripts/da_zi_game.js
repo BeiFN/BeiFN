fa085e0c74359ab7ba3cbebf3ad127c7543b8f4d
张朋伟 <3316756985@qq.com>
2019-06-23 15:37:31 +0800

    0623提交

--- group1/张朋伟/0622/打字游戏/javascripts/da_zi_game.js
+++ group1/张朋伟/0622/打字游戏/javascripts/da_zi_game.js
@@ -4,50 +4,59 @@ var client_width = document.documentElement.clientWidth;
 
 
 // 创建鱼
+setInterval(createFish, 1000);
+
 function createFish(evt) {
-    var e = evt || event;
+    // var e = evt || event;
     var fish = document.createElement("div");
     fish.id = "fish";
     var span = document.createElement("span");
+    //调用创造字母函数
     span.id = "f_span";
-    fish.children(span);
-    
+    fish.appendChild(span);
+    document.body.appendChild(fish);
+    //获得随机字母
 
+    span.innerHTML = randomLetter();
+    fishMove(fish);
 }
+
+
 
 //鱼获得随机字母
 function randomLetter() {
     var shu = 65 + Math.round(Math.random() * 25)
     var letter = String.fromCharCode(shu);
-    console.log(letter);
-    fish.children[0].innerHTML = letter;
+    return letter;
+    // console.log(fish.children[0].innerHTML);
 }
 //鱼移动
 // console.log(rand_left);
-fishMove();
-// rand_top = fish.offsetTop;
-console.log(fishTop);
 
-function fishMove() {
-    fishTop = 0;
+// rand_top = fish.offsetTop;
+// console.log(fishTop);
+
+function fishMove(ele) {
+
+    var numTop = 0;
     var timer = null;
-    timer = setInterval(function () {
-        fishTop += 5;
-        fish.style.top = fishTop + "px";
-        rand_left = Math.round(Math.random() * (client_width - 160));
-        fish.style.left = rand_left + "px";
-        if (fishTop > 500) {
-            clearInterval(timer);
-            timer = null;
-            fish.className = "die";
-            setTimeout(
-                function () {
-                    fish.remove();
-                }
-            ), 1000
-        }
-        return fishTop;
-    }), 1000
+    timer = setInterval( function (){
+          numTop += 1;
+        //   ele.style.left = Math.round(Math.random()*document.documentElement.clientWidth -160)+"px";
+          ele.style.top = numTop + "px";
+          if(numTop >= 500){
+                clearInterval(timer);
+                // 清空变量，让变量内的数据被垃圾回收机制回收;
+                timer = null;
+                ele.className += " die";
+                // 动画运行结束删除元素;
+                setTimeout( function (){
+                      removeRecord(ele)
+                      ele.remove();
+                } ,1000)
+          }
+    },50)
+    return timer;
 
 }
 
@@ -55,12 +64,13 @@ function fishMove() {
 function onkeydown(evt) {
 
     handlerEnterLetter();
-    createBullet(evt);
+
+    createFish(evt);
 
 
 }
 
-function handlerEnterLetter() {
+function handlerEnterLetter(evt) {
     var e = evt || event;
     var onkeydown = e.keyCode || e.witch;
     if (code >= 65 && code <= 90) {
@@ -68,6 +78,7 @@ function handlerEnterLetter() {
         pao.children[0] = code;
     } else return false;
 }
+
 // function createBullet(evt){
 //     var e = evt || event;
 //     var fishes = document.createElement("div");
