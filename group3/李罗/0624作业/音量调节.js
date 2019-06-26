2e6ab2516267d9a8e1677abdc6a45a13770fe36f
SLA0630 <1515693903@qq.com>
2019-06-26 08:55:22 +0800

    update homework

--- group3/李罗/0624作业/音量调节.js
+++ group3/李罗/0624作业/音量调节.js
@@ -5,7 +5,7 @@ var horn=$(".horn");
 var GLOBAL={
     proOffsetLeft:progress.offsetLeft,
     proWidth:progress.offsetWidth,//包含边框
-    volume:{
+    volume : {
         "low" : "https://upload-images.jianshu.io/upload_images/16960494-86ea623e30ad4c2d.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240",
             "hight": "https://upload-images.jianshu.io/upload_images/16960494-0192f12b7372e21e.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" ,
             "mute": "https://upload-images.jianshu.io/upload_images/16960494-e859bac6f34f9188.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" ,
@@ -16,7 +16,7 @@ var GLOBAL={
 
 function $(selector){
     ele=null;
-    return (ele=document.querySelectorAll().length===1?ele[0]:ele);
+    return (ele=document.querySelectorAll(selector)).length===1?ele[0]:ele;
 }
 
 //point 的拖拽
@@ -30,21 +30,48 @@ function handlerVolumeChangeMove(evt){
     var e=evt||window.event;
     //根据鼠标位置,设置当前元素位置; 根据点击位置计算正确元素位置;
     var left=e.clientX-GLOBAL.X-GLOBAL.proOffsetLeft;
+    // console.log(left);
 
     //检测边界；
-    left=left<-15?-15:left;
-    left=left>=GLOBAL.proWidth-15?GLOBAL.proWidth:left;
+    left=left < -15 ? -15 : left;
+    left= left >= GLOBAL.proWidth - 15 ?GLOBAL.proWidth - 15:left;
 
     progressPoint.style.left=left+"px";
+    var prop=changeProgressBarWidth(left);
 
-    
+    progress.title=prop+"%";
+    changeVolumeIcon(prop);
+}
+function changeVolumeIcon(prop){
+    if(prop===0){
+        horn.style.backgroundImage="url("+GLOBAL.volume.mute+")";
+    }else if(prop>0&&prop<33){
+        horn.style.backgroundImage="url("+GLOBAL.volume.low+")";
+    }else if(prop>=33&&prop<66){
+        horn.style.backgroundImage = "url("+GLOBAL.volume.middle+")";
+    }else{
+        horn.style.backgroundImage = "url("+GLOBAL.volume.hight+")";
+    }
+}
 
+function handlerVolumeChangeEnd(){
+    document.removeEventListener("mousemove",handlerVolumeChangeMove);
+}
+
+function changeProgressBarWidth(_left){
+    progressBar.style.width=_left+15+"px";
+    return parseInt((_left+15)/GLOBAL.proWidth*100);
 }
 
 
 
 
 
+progressPoint.addEventListener("mousedown",handlerVolumeChangeStart);
+document.addEventListener("mouseup",handlerVolumeChangeEnd)
 
-progressPoint.addEventListener("mousemove",handlerVolumeChangeStart);
-document.addEventListener("mouseup",handlerDragEnd)
+//阻止区域中无用的默认事件
+progress.mousedown=function(evt){
+    var e=evt||window.event;
+    e.preventDefault();
+}
