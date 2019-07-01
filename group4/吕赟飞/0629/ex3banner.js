c0a6aca4fbe0bba652297851d0fb26fc53ca0f12
吕赟飞 <lvyunf3@users.noreply.github.com>
2019-07-01 08:13:22 +0800

    吕赟飞0701作业修改

---
+++ group4/吕赟飞/0629/ex3banner.js
@@ -0,0 +1,64 @@
+var index = 1;
+
+var next_btn = $(".button-next");
+var prev_btn = $(".button-prev");
+var sliders = $(".slider");
+var wrapper = $(".wrapper");
+var container = $(".container");
+var liList = Array.from($("li"));
+
+function $(selector) {
+    var ele = null;
+    return (ele = document.querySelectorAll(selector)).length === 1 ? ele[0] : ele;
+}
+
+for (var i = 0; i < liList.length; i++) {
+    liList[i].addEventListener("click", liClickHandler);
+}
+
+function liClickHandler() {
+    index = liList.indexOf(this) + 1;
+    imgMove(-1280 * index, wrapper, "left");
+}
+
+next_btn.addEventListener("click", function () {
+    if (index === sliders.length - 1) {
+        index = 2;
+        wrapper.style.left = "-1280px";
+    } else {
+        index++;
+    }
+});
+
+prev_btn.addEventListener("click", function () {
+    if (index === 0) {
+        index = sliders.length - 3;
+        wrapper.style.left = "-6400px";
+    } else {
+        index--;
+    }
+});
+
+
+container.addEventListener("click", function (evt) {
+    var e = evt || window.event;
+    var target = e.target || e.srcElement;
+    if (target === next_btn || prev_btn) {
+        imgMove(-1280 * index, wrapper, "left");
+    }
+});
+
+
+function imgMove(target, dom, attr) {
+    clearInterval(dom.timer);
+    dom.timer = setInterval(function () {
+        var iNow = parseInt(getComputedStyle(dom)[attr]);
+        var speed = (target - iNow) / 10;
+        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+        if (target === iNow) {
+            clearInterval(dom.timer);
+        } else {
+            dom.style[attr] = iNow + speed + "px";
+        }
+    }, 16);
+}
