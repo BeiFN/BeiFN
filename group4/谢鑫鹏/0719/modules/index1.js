331d51f6ef0a73b3f82eb08f9f9602972c40a278
xxp13579 <48877737+xxp13579@users.noreply.github.com>
2019-07-19 23:42:17 +0800

    谢鑫鹏0719作业提交

---
+++ group4/谢鑫鹏/0719/modules/index1.js
@@ -0,0 +1,17 @@
+// let a = { name : "a"};
+// let b = { name : "b"};
+
+// export{
+//       a,
+//       b
+// }
+
+let name = "name";
+export{
+  name
+}
+
+// export let name;
+// export let age;
+// name = "name";
+// age = "age";
