d5dad779d966d95ebaadadc60e84de7e2426c34e
InuYashaDo <986841218@qq.com>
2019-07-23 08:39:38 +0800

    作业提交

---
+++ group2/郭钢/0721/zcool_web_project/dist/scripts/index.js
@@ -0,0 +1,1 @@
+"use strict";function _classCallCheck(a,n){if(!(a instanceof n))throw new TypeError("Cannot call a class as a function")}var a=function a(){_classCallCheck(this,a),this.a="hehe"};new a,console.log("haode");
